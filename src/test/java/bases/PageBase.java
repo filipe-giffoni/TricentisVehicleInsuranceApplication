3c0f4536c998816c082a4878cf4a2c85662a61d1
Filipe Giffoni Gomes <filipe_giffoni@hotmail.com>
2021-04-03 23:29:13 -0300

    TestRunners added, testing framework changed to JUnit

--- src/test/java/bases/PageBase.java
+++ src/test/java/bases/PageBase.java
@@ -1,12 +1,12 @@
 package bases;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 
 public class PageBase {
 
@@ -43,7 +43,7 @@ public class PageBase {
 
     protected void assertEquals(WebElement element, String text) {
         waitForElement(element);
-        Assert.assertEquals(element.getText(), text);
+        Assert.assertEquals(text, element.getText());
     }
 
 }
